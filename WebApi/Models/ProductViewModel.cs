13e7e64adcda8b50499fbdb54678cffdd54ebcb2
Nguyen Ba Dung <nbdung@hyperion.co.jp>
2022-06-13 23:10:37 +0700

    Update PJ

---
+++ WebApi/Models/ProductViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class ProductViewModel
+    {
+        public long ID { get; set; }
+
+        [StringLength(250)]
+        public string Name { get; set; }
+
+        [StringLength(10)]
+        public string Code { get; set; }
+
+        [StringLength(250)]
+        public string MetaTitle { get; set; }
+
+        public decimal? Price { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
